f3d40750a50ebcd7020d69c78e7f5d564ba24a9f
adel-daniel <adel.daniel.222@gmail.com>
2026-05-25 15:31:43 +0300

    Complete the task

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    var coinManager: CoinManager = CoinManager()
+    
+    
+    var currencyArray: [String] {
+        coinManager.currencyArray
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+        coinManager.delegate = self
     }
-
-
 }
 
+
+
+// MARK: - UIPickerViewDelegate & UIPickerViewDataSource
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    // Number of columns (wheels)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Number of items in the column
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    // Text for each row
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedValue = currencyArray[row]
+        coinManager.getCoinPrice(for: selectedValue)
+        print("User selected: \(selectedValue)")
+    }
+}
+
+// MARK: - CoinManagerDelegate
+extension ViewController: CoinManagerDelegate {
+    
+    func didUpdateCoinPrice(_ coinManager: CoinManager, price: String, currency: String) {
+        //Remember that we need to get hold of the main thread to update the UI, otherwise our app will crash if we
+        //try to do this from a background thread (URLSession works in the background).
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: any Error) {
+        print(error)
+    }
+    
+    
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,93 @@
 
 import Foundation
 
+/// Create the delegate at the same file that use that delegate
+/// With the same name + Delegate
+protocol CoinManagerDelegate {
+    //Create the method stubs wihtout implementation in the protocol.
+    //It's usually a good idea to also pass along a reference to the current class.
+    //e.g. func didUpdatePrice(_ coinManager: CoinManager, price: String, currency: String)
+    //Check the Clima module for more info on this.
+    func didUpdateCoinPrice(_ coinManager: CoinManager, price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
+    var selectedCurrency: String = "USD"
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "YOUR_API_KEY_HERE"
-    
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    
+    mutating func getCoinPrice(for currency: String){
+        let stringUrl = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        selectedCurrency = currency
+        performRequest(stringUrl: stringUrl)
+        print(stringUrl)
+    }
+    
+    func performRequest(stringUrl: String) {
+        /// 1. Create a URL
+        if let url = URL(string: stringUrl) {
+            
+            /// 2. Create a URL Session
+            let session = URLSession(configuration: .default)
+            
+            /// 3. Create a Data Task
+            let task = session.dataTask(with: url, completionHandler: _handler)
+            
+            /// 4. Start Task
+            task.resume()
+        }
+    }
+    
+    func _handler(data: Data?, response: URLResponse?, error: Error?) -> Void{
+        /// 5. Check Error
+        if error != nil {
+            delegate?.didFailWithError(error: error!)
+            print(error!)
+            return
+        }
+        
+        
+        /// 6. Check Data And Parse
+        if let data = data {
+            print(data)
+            if let price = parseJSON(data){
+                //Optional: round the price down to 2 decimal places.
+                let priceString = String(format: "%.2f", price)
+                
+                //Call the delegate method in the delegate (ViewController) and
+                //pass along the necessary data.
+                self.delegate?.didUpdateCoinPrice(self, price: priceString, currency: selectedCurrency)
+            }
+        }
+        
+    }
+    
+    
+    func parseJSON(_ data: Data) -> Double? {
+        
+        //Create a JSONDecoder
+        let decoder = JSONDecoder()
+        do {
+            //try to decode the data using the CoinData structure
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            
+            //Get the last property from the decoded data.
+            let lastPrice = decodedData.rate
+            print(lastPrice)
+            return lastPrice
+            
+        } catch {
+            //Catch and print any errors.
+            delegate?.didFailWithError(error: error)
+            print(error)
+            return nil
+        }
+    }
+    
     
 }
